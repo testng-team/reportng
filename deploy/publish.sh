66fc07fde7d5fdf77b07539aae83016422d23eaf
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2018-07-06 09:08:56 +0530

    Ensure Maven deploys only when built with JDK8
    Closes #3

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
+if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ] ; then
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" ] ; then
+    if [ "${TRAVIS_BRANCH}" == "master" ] ; then
+      mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+      exit $?
+    else
+      echo "Deploy skipped: Only the master branch"
+    fi
+  else
+    echo "Deploy skipped: On pull request"
+  fi
+else
+  echo "Deploy skipped: Only on Oracle oraclejdk8"
 fi
