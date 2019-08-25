8299bb18ddc95472f343e137be6641b5b4ad2428
Cedric Beust <cedric@beust.com>
2019-08-25 09:30:21 -0700

    Publishing scripts.

---
+++ deploy/publishToMavenCentral.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ] ; then
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" ] ; then
+    if [ "${TRAVIS_BRANCH}" == "master" ] ; then
+      ./gradlew publish
+      exit $?
+    else
+      echo "Deploy skipped: Only the master branch"
+    fi
+  else
+    echo "Deploy skipped: On pull request"
+  fi
+else
+  echo "Deploy skipped: Only on Oracle oraclejdk8"
+fi
